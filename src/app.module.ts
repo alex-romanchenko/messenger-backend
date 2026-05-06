e9ccc84842877c1a71bb839645fdfc34c8b18b5f
Romanchenko Oleksandr <romanchenko.sawa@gmail.com>
2026-05-06 18:07:57 +0300

    fixing app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
 import { Message } from './messages/message.entity';
@@ -39,5 +42,7 @@ import { ChatMember } from './chat/chat-member.entity';
     UsersModule,
     ContactsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
